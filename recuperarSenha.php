72a368e67be91e87edbdcb348a01ae194bd80176
aerther <arthurlassem11@gmail.com>
2025-10-09 17:20:41 -0300

    Recuperar Senha

---
+++ recuperarSenha.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+    if(!isset($_SESSION['$senha'])){
+        $_SESSION['$senha']=0;//Alterar depois
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Senha</title>
+</head>
+<body>
+    <form action='recuperarSenha.php' method='post'>
+        <label for='email'>E-mail:</label>
+        <input type='email' name='email' id='email' required>
+        <input type='submit' name='recuperar' value='Recuperar'>
+    </form>
+    <p><?php echo htmlspecialchars($senha); ?></p>
+    <a href="index.php"><button>Voltar</button></a>
+</body>
+</html>
