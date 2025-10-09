dfe8bdb59ddb2fa88d39260ff63f52a9d56776b8
Kelly Larissa Thomas <kellylthomas60@gmail.com>
2025-10-09 15:41:42 -0300

    htmltelalogin

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estagirando</title>
+</head>
+<body>
+
+    <h1>Bem-vindo ao Estagirando</h1>
+
+    <form action="" method="Post">
+    <label for="email">
+        E-mail<input type="text" name="email" id="email">
+    </label>
+    <label for="senha">
+        Senha <input type="text" name="senha" id="senha">
+    </label>
+
+    <input type="submit" value="Entrar" name="botao">
+
+    <a href="recuperarsenha.php">Recuperar Senha</a>
+
+    <a href="cadastroprofessor.php">Cadastrar-se como professor</a>
+
+    <a href="cadastroaluno.php">Cadastrar-se como aluno</a>
+
+
+    </form>
+    
+
+</body>
+</html>
