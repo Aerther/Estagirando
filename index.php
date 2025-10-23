5301e50670490939f069dd7cb7b5e5111eeec923
unknown <jesperaficosipp@gmail.com>
2025-10-23 17:36:34 -0300

    páginas

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ if(isset($_POST["botao-enviar"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="./src/styles/reset.css">
     <link rel="stylesheet" href="./src/styles/style.css">
 
     <script src="./src/js/main.js" defer></script>
