25a73ef866fab2650bb8d38fc1a2f32d94c13619
aerther <arthurlassem11@gmail.com>
2025-10-23 09:12:10 -0300

    Adicionando Vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'grupo1/estagirando',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d313149b6a2c7cba6ca01bca9bddb6f772d32f4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'grupo1/estagirando' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d313149b6a2c7cba6ca01bca9bddb6f772d32f4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
