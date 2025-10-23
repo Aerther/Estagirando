b4d0dbb18b106c40b0771b1171c33247b63a703c
unknown <jesperaficosipp@gmail.com>
2025-10-23 16:36:13 -0300

    páginas

---
+++ editarCadastro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edição de Cadastro</title>
+</head>
+<body>
+    <div id="menu">
+        <a href="pesquisa.php">Pesquisa</a>
+        <a href="solicitacoesOrientacao.php">Solicitações</a>
+        <a href="visualizarCadastro.php">Próprio Cadastro</a>
+        <a href="editarCadastro.php">Edição de Cadastro</a>
+        <a href="sair.php">Sair</a>
+    </div>
+    </div>
+    
+</body>
+</html>
