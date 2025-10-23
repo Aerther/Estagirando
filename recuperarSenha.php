9a55e4ba8c597317d675c6cc6ce9595f53247655
unknown <jesperaficosipp@gmail.com>
2025-10-23 17:02:21 -0300

    páginas

--- recuperarSenha.php
+++ recuperarSenha.php
@@ -7,7 +7,7 @@ use App\Classes\Usuario;
 $mensagem = "";
 $novaSenha = "";
 
-if (isset($_POST["recuperar"])) {
+if (isset($_POST["recuperar"]) ) {
     $usuario = new Usuario($_POST["email"], "");
 
     if (!$usuario->usuarioExiste()) {
@@ -28,22 +28,20 @@ if (isset($_POST["recuperar"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/styles/reset.css">
+
+    <script src="./src/js/main.js" defer></script>
     <title>Estagirando</title>
 </head>
 <body>
     <div class="container">
-        <h1>Esqueceu a sua senha?</h1>
-        <h2>Redefina a sua senha aqui!</h2>
+        <h1 id="titulo1">Esqueceu a sua senha?</h1>
+        <h2 id="subtitulo">Redefina a sua senha aqui!</h2>
 
         <form action="./recuperarSenha.php" method="post">
             <section>
                 <label for="email">Informe seu E-mail:</label>
                 <input type="email" name="email" id="email" required>
-            </section>
-
-            <section>
-                <a href="index.php">Voltar</a>
-                <input type="submit" name="recuperar" value="Recuperar">
             </section>
 
             <section>
@@ -53,7 +51,15 @@ if (isset($_POST["recuperar"])) {
             <section>
                 <?php if($novaSenha) echo "Nova Senha: " . $novaSenha; ?>
             </section>
+
+            <section>
+                
+                <input type="submit" name="recuperar" value="Recuperar">
+            </section>
+
         </form>
     </div>
+    <a href="index.php">Voltar</a>
+
 </body>
 </html>
