54c75148d0566d31e1d87bb43cf4f9ca3bf82e4a
unknown <jesperaficosipp@gmail.com>
2025-10-26 09:01:02 -0300

    icones

--- pages/pesquisa/pesquisa.php
+++ pages/pesquisa/pesquisa.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pesquisa Avançada</title>
+    <link rel="stylesheet" href="./../../src/styles/styleMenu.css">
 </head>
 <body>
     <div id="menu">
-        <a href="./../pesquisa/pesquisa.php">Pesquisa</a>
-        <a href="./../../solicitacoesOrientacao.php">Solicitações</a>
-        <a href="./../visualizar/visualizarCadastro.php">Próprio Cadastro</a>
-        <a href="./../editar/editarCadastro.php">Edição de Cadastro</a>
-        <a href="./../../sair.php">Sair</a>
+        <div id='saudacao'>
+            <h1>Olá, ...</h1>
+
+        </div>
+        <div id='icone'>
+            <a href="./../pesquisa/pesquisa.php"><img src="./../../icones/pesquisa.png" alt="" id='iconeMenu'></a>
+            <a href="./../../solicitacoesOrientacao.php"><img src="./../../icones/solicitacoes.png" alt="" id='iconeMenu'></a>
+            <a href="./../editar/editarCadastro.php"><img src="./../../icones/edicao.png" alt="" id='iconeMenu'></a>   
+            <a href="./../visualizar/visualizarCadastro.php"><img src="./../../icones/iconProf.png" alt="" id='iconeMenu'></a>
+            <a href="./../../sair.php"><img src="./../../icones/logout.png" alt="" id='iconeMenu'></a>
+
+        </div>
+        
     </div>
     
 </body>
