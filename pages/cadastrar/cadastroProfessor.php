401333cd15939b72079d8d87f323b09b88960db8
unknown <jesperaficosipp@gmail.com>
2025-10-28 20:21:27 -0300

    css

--- pages/cadastrar/cadastroProfessor.php
+++ pages/cadastrar/cadastroProfessor.php
@@ -72,7 +72,7 @@ foreach ($preferencias as $preferencia) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estagirando</title>
-    <link rel="stylesheet" href="./../src/styles/styleCadastro.css">
+    <link rel="stylesheet" href="../../src/styles/styleCadastro.css">
 
 </head>
 <body>
