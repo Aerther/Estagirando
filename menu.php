68dc67c89cb4666499b355b582a4164f755d1ff9
joice-2008 <jesperaficosipp@gmail.com>
2025-12-02 14:51:26 -0300

    titulos

--- menu.php
+++ menu.php
@@ -20,7 +20,7 @@ $lvc = ($_SESSION["tipoUsuario"] == "aluno" ? "visualizarAluno.php" : "visualiza
 
 <header>
     <section class="texto-inicial">
-        <?php echo "<p>Bem-vindo {$_SESSION['tipoUsuario']}!</p>"; ?>
+        <?php echo "<p>Bem-vindo {$_SESSION['tipoUsuario']}</p>"; ?>
     </section>
 
     <section class="icones">
